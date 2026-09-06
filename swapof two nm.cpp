36878bc77a3eecc2376e5c791a47813315837b78
Prithviraaj Meher <prithviraajmeher05@gmail.com>
2026-09-06 19:01:59 +0530

    Add files via upload

---
+++ swapof two nm.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=2,b=30;
+	cout<<"value of a ="<<a<<ends<<"b="<<b<<endl;
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	cout<<"value of a="<<a<<ends<<"b="<<b;
+	return 0;
+}
